07f99232b4f2408f4db2b8bffe6d8b44b5241d21
Adrian.Conlon <adrian.conlon@gmail.com>
2016-07-12 19:27:19 +0100

    Simplify the hell out of the signals code, speeds things up loads!
    Signed-off-by: Adrian.Conlon <adrian.conlon@gmail.com>

--- app/Signal.ts
+++ app/Signal.ts
@@ -33,51 +33,7 @@ export class Signal {
      */
     public static VERSION: string = "1.0.0";
 
-    /**
-     * If Signal should keep record of previously dispatched parameters and
-     * automatically execute listener during `add()`/`addOnce()` if Signal was
-     * already dispatched before.
-     * @type boolean
-     */
-    public memorize: boolean = false;
-
-    /**
-     * If Signal is active and should broadcast events.
-     * <p><strong>IMPORTANT:</strong> Setting this property during a dispatch will only affect the next dispatch,
-     * if you want to stop the propagation of a signal use `halt()` instead.</p>
-     * @type boolean
-     */
-    public active: boolean = true;
-
     private _bindings: SignalBinding[] = [];
-
-    private _prevParams: any = undefined;
-
-    private _shouldPropagate: boolean = true;
-
-    /**
-     * Check if listener was attached to Signal.
-     * @param {Function} listener
-     * @param {Object} [context]
-     * @return {boolean} if Signal has the specified listener.
-     */
-    public has(listener: Function, context?: any): boolean {
-        return this._indexOfListener(listener, context) !== -1;
-    }
-
-    /**
-     * Add listener to the signal that should be removed after first execution (will be executed only once).
-     * @param {Function} listener Signal handler function.
-     * @param {Object} [listenerContext] Context on which listener will be executed (object that should represent the
-     * `this` variable inside listener function).
-     * @param {Number} [priority] The priority level of the event listener. Listeners with higher priority will be
-     * executed before listeners with lower priority. Listeners with same priority level will be executed at the
-     * same order as they were added. (default = 0)
-     * @return {SignalBinding} An Object representing the binding between the Signal and listener.
-     */
-    public addOnce(listener: Function, listenerContext?: any, priority?: number): SignalBinding {
-        return this._registerListener(listener, true, listenerContext, priority);
-    }
 
     /**
      * Add a listener to the signal.
@@ -91,24 +47,6 @@ export class Signal {
      */
     public add(listener: Function, listenerContext?: any, priority?: number): SignalBinding {
         return this._registerListener(listener, false, listenerContext, priority);
-    }
-
-    /**
-     * Remove a single listener from the dispatch queue.
-     * @param {Function} listener Handler function that should be removed.
-     * @param {Object} [context] Execution context (since you can add the same handler multiple times if executing in a different context).
-     * @return {Function} Listener handler function.
-     */
-    public remove(listener: Function, context?: any): Function {
-
-        let i: number = this._indexOfListener(listener, context);
-
-        if (i !== -1) {
-            this._bindings[i]._destroy(); // no reason to a SignalBinding exist if it isn't attached to a signal
-            this._bindings.splice(i, 1);
-        }
-
-        return listener;
     }
 
     /**
@@ -133,52 +71,19 @@ export class Signal {
     }
 
     /**
-     * Stop propagation of the event, blocking the dispatch to next listeners on the queue.
-     * <p><strong>IMPORTANT:</strong> should be called only during signal dispatch, calling it before/after
-     * dispatch won't affect signal broadcast.</p>
-     * @see Signal.prototype.disable
-     */
-    public halt(): void {
-        this._shouldPropagate = false;
-    }
-
-    /**
      * Dispatch/Broadcast Signal to all listeners added to the queue.
      * @param {...*} [paramsArr] Parameters that should be passed to each handler.
      */
     public dispatch(...paramsArr: any[]): void {
 
-        if (!this.active) {
-            return;
-        }
-
         let n: number = this._bindings.length;
-        let bindings: SignalBinding[];
-
-        if (this.memorize) {
-            this._prevParams = paramsArr;
-        }
-
-        if (!n) {
-            // should come after memorize
+        if (n === 0) {
             return;
         }
 
-        bindings = this._bindings.slice(0); // clone array in case add/remove items during dispatch
-
-        this._shouldPropagate = true; // in case `halt` was called before dispatch or during the previous dispatch.
-
-        // execute all callbacks until end of the list or until a callback returns `false` or stops propagation
-        // reverse loop since listeners with higher priority will be added at the end of the list
-        do { n--; } while (bindings[n] && this._shouldPropagate && bindings[n].execute(paramsArr) !== false);
-    }
-
-    /**
-     * Forget memorized arguments.
-     * @see Signal.memorize
-     */
-    public forget(): void {
-        this._prevParams = undefined;
+        for (let i: number = 0; i < n; ++i) {
+            this._bindings[i].execute(paramsArr);
+        }
     }
 
     /**
@@ -188,43 +93,21 @@ export class Signal {
     public dispose(): void {
         this.removeAll();
         delete this._bindings;
-        delete this._prevParams;
     }
 
     /**
      * @return {string} String representation of the object.
      */
     public toString(): string {
-        return "[Signal active:" + this.active + " numListeners:" + this.getNumListeners() + "]";
+        return "[numListeners:" + this.getNumListeners() + "]";
     }
 
     private _registerListener(listener: Function, isOnce: boolean, listenerContext: Object, priority: number): SignalBinding {
 
-        let prevIndex: number = this._indexOfListener(listener, listenerContext);
-        let binding: SignalBinding;
-
-        if (prevIndex !== -1) {
-            binding = this._bindings[prevIndex];
-
-            if (binding.isOnce() !== isOnce) {
-                throw new Error(
-                    "You cannot add"
-                    + (isOnce ? "" : "Once")
-                    + "() then add"
-                    + (!isOnce ? "" : "Once")
-                    + "() the same listener without removing the relationship first.");
-            }
-        } else {
-            binding = new SignalBinding(this, listener, isOnce, listenerContext, priority);
-            this._addBinding(binding);
-        }
-
-        if (this.memorize && this._prevParams) {
-            binding.execute(this._prevParams);
-        }
+        let binding: SignalBinding = new SignalBinding(this, listener, isOnce, listenerContext, priority);
+        this._addBinding(binding);
 
         return binding;
-
     }
 
     private _addBinding(binding: SignalBinding): void {
@@ -232,18 +115,5 @@ export class Signal {
         let n: number = this._bindings.length;
         do { --n; } while (this._bindings[n] && binding.priority <= this._bindings[n].priority);
         this._bindings.splice(n + 1, 0, binding);
-    }
-
-    private _indexOfListener(listener: Function, context: Object): number {
-
-        let n: number = this._bindings.length;
-        while (n--) {
-            let cur: SignalBinding = this._bindings[n];
-            if (cur.getListener() === listener && cur.context === context) {
-                return n;
-            }
-        }
-
-        return -1;
     }
 }
